09a8d76c718ac19c59762a39bae4766edb69ff23
Jenifer <jenyspa@gmail.com>
2020-08-26 22:33:31 -0300

    feat(App) set action on login button

--- lifeshare/App.js
+++ lifeshare/App.js
@@ -1,24 +1,42 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import axios from "axios";
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Life Share</Text>
-        <View>
-          <Text>Login</Text>
-          <TextInput style={styles.input} />
-          <Text style={{ marginTop: 20 }}>Senha</Text>
-          <TextInput style={styles.input} />
-        </View>
-        <View>
-          <Button title="ENTRAR" color="#E84C0E" />
-          <Text style={styles.signUp}>Cadastre-se</Text>
-        </View>
-      </View>
-    );
+export default function App() {
+  const [email, setEmail] = useState("")
+  const [password, setPass] = useState("")
+
+  function handleChangeEmail(value) {
+    setEmail(value)
   }
+
+  function handleChangePass(value) {
+    setPass(value)
+  }
+
+  function login() {
+    axios.post('https://google.com', {email, password}).then(function() {
+      console.log("Success")
+    }).catch(function(error) {
+      console.log(error)
+    })
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Life Share</Text>
+      <View>
+        <Text>Login</Text>
+        <TextInput style={styles.input} onChangeText={handleChangeEmail}/>
+        <Text style={{ marginTop: 20 }}>Senha</Text>
+        <TextInput style={styles.input} onChangeText={handleChangePass}/>
+      </View>
+      <View>
+        <Button title="ENTRAR" color="#E84C0E" onPress={login}/>
+        <Text style={styles.signUp}>Cadastre-se</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({

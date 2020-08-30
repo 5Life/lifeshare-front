d3b5b3124c79abdbe5e564533b5d907887d45fd4
Jenifer <jenyspa@gmail.com>
2020-08-30 19:05:17 -0300

    feat(App): create register layout

--- lifeshare/App.js
+++ lifeshare/App.js
@@ -1,64 +1,19 @@
-import React, { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-import axios from "axios";
+import 'react-native-gesture-handler'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+const Stack = createStackNavigator(); 
 
 export default function App() {
-  const [email, setEmail] = useState("")
-  const [password, setPass] = useState("")
-
-  function handleChangeEmail(value) {
-    setEmail(value)
-  }
-
-  function handleChangePass(value) {
-    setPass(value)
-  }
-
-  function login() {
-    axios.post('https://google.com', {email, password}).then(function() {
-      console.log("Success")
-    }).catch(function(error) {
-      console.log(error)
-    })
-  }
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Life Share</Text>
-      <View>
-        <Text>Login</Text>
-        <TextInput style={styles.input} onChangeText={handleChangeEmail}/>
-        <Text style={{ marginTop: 20 }}>Senha</Text>
-        <TextInput style={styles.input} onChangeText={handleChangePass}/>
-      </View>
-      <View>
-        <Button title="ENTRAR" color="#E84C0E" onPress={login}/>
-        <Text style={styles.signUp}>Cadastre-se</Text>
-      </View>
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Dados Pessoais" component={Register}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 30,
-    backgroundColor: "#fff",
-    justifyContent: "space-around",
-  },
-  title: {
-    fontSize: 30,
-    textAlign: "center",
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#E84C0E",
-    height: 30,
-    marginTop: 8,
-  },
-  signUp: {
-    textDecorationLine: "underline",
-    textAlign: "center",
-    marginTop: 8,
-  },
-});
---
+++ lifeshare/pages/Login/Login.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import axios from 'axios'
+import { NavigationContainer } from '@react-navigation/native'
+
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState('')
+  const [password, setPass] = useState('')
+
+  function login() {
+    axios
+      .post('localhost:8080/api/auth', { email, password })
+      .then(function () {
+        console.log('Success')
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Life Share</Text>
+      <View>
+        <Text>Login</Text>
+        <TextInput style={styles.input} onChangeText={setEmail} />
+        <Text style={{ marginTop: 20 }}>Senha</Text>
+        <TextInput style={styles.input} onChangeText={setPass} />
+      </View>
+      <View>
+        <Button title='ENTRAR' color='#E84C0E' onPress={login} />
+        <Text style={styles.signUp} onPress={() => navigation.navigate('Dados Pessoais')}>Cadastre-se</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30,
+    backgroundColor: '#fff',
+    justifyContent: 'space-around',
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#E84C0E',
+    height: 30,
+    marginTop: 8,
+  },
+  signUp: {
+    textDecorationLine: 'underline',
+    textAlign: 'center',
+    marginTop: 8,
+  }
+})

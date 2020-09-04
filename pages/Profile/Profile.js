8692c7abf1a593c072ba66d6df8d9c8bf92bbef0
Patricia Clares <patricia.clares@dasa.com.br>
2020-09-04 00:20:45 -0300

    add profile style

--- pages/Profile/Profile.js
+++ pages/Profile/Profile.js
@@ -1,22 +1,37 @@
 import React from 'react'
-import { Text, View, Dimensions, StyleSheet } from 'react-native'
-
-var { height } = Dimensions.get('window');
-
-var box_count = 2;
-var box_height = height / box_count;
+import { Text, View, StyleSheet, Image } from 'react-native'
 
 export default function Profile({ navigation }) {
   return(
-    <View style={styles.container}>
-      <View style={[styles.box, styles.profile]}>
-        <Text style={styles.data}>Andrezinho</Text>
-        <Text style={styles.data}>O+</Text>
-        <Text style={styles.data}>Órgãos</Text>
-        <Text style={styles.data}>Sangue</Text>
+    <View style={[styles.main,styles.container]}>
+      <View style={[styles.main,styles.container, styles.profile]}>
+        <View style={styles.main}>
+          <Image 
+            source={require('../../assets/Ellipse2.png')}
+            style={{minHeight: 150, minWidth: 150, margin: 10
+            }}/>
+          <View style={[styles.main, styles.row]}>
+            <Text style={styles.data}>Andrezinho</Text>
+            <Text style={styles.data}> O+</Text>
+            <Image source={require('../../assets/Ellipse8.png')}
+            style={{width:30, height:30, marginLeft:10
+            }}/>
+          </View>
+        </View>
+        <View style={styles.basicData}>
+            <Text style={styles.data}>Órgãos</Text>
+            <Text style={styles.data}>Sangue</Text>
+        </View>
       </View>
-      <View style={[styles.box, styles.emptyBox]}>
-        <Text style={styles.empty}>Você ainda não tem doações cadastradas</Text>
+      <View style={[styles.main,styles.container]}>
+        <Text style={styles.empty}>Você ainda não tem doações cadastradas</Text>  
+        <Image source={require('../../assets/semDoacoes.png')}
+            style={{minHeight:80, minWidth:80, marginTop:10}}/>
+        <View style={styles.button}>
+          <Image 
+            style={{width:45, height:45}} 
+            source={require('../../assets/buttonAdd.png')}/>
+        </View>
       </View>
     </View>
   )
@@ -24,26 +39,37 @@ export default function Profile({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
+    flex:1,
+    width:"100%"
+  },
+  main: {
     display: "flex",
-    flex: 1,
-    flexDirection: 'column'
-  },
-  box: {
-    display: "flex",
-    height: box_height
-  },
-  profile: {
-    backgroundColor: "#E84C0E"
-  },
-  data: {
-    fontSize: 30,
-    color: "white"
-  },
-  emptyBox: {
     justifyContent: "center",
     alignItems: "center"
   },
+  profile: {
+    backgroundColor: "#E84C0E",
+  },
+  data: {
+    fontSize: 25,
+    color: "white"
+  },
   empty: {
-    fontSize: 20
+    fontSize: 18,
+  },
+  row: {
+    flexDirection: "row"
+  },
+  basicData:{
+    display:"flex", 
+    flexDirection:"row", 
+    justifyContent:"space-around", 
+    width:"100%",
+    marginTop:20
+  },
+  button: {
+    position: "absolute", 
+    bottom:20, 
+    right:10
   }
 })
